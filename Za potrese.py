48396fd359edc898916b5022836d6044c049e460
tjasabajc <tjasa.bajc@student.fmf.uni-lj.si>
2016-10-24 11:51:38 +0200

    Bolj ali manj končna, zna naredit csv! :)

--- Za potrese.py
+++ Za potrese.py
@@ -13,13 +13,52 @@ def iskanje():
     regex_potresa = re.compile(
         r'<tr id="(?P<id>(\d{6}))".*?'
         r'>(?P<leto>(\d{4}))-(?P<mesec>(\d{2}))-(?P<dan>(\d{2})).*?'
-        r';(?P<ura>(\d{2})):(?P<minuta>(\d{2})):(?P<sekunda>(\d{2}.\d))<',
-        #;07:17:26.5</a></b></td><td class="tabev1">38.17&nbsp;</td><td class="tabev2">N&nbsp;&nbsp;</td><td class="tabev1">26.57&nbsp;</td><td class="tabev2">E&nbsp;&nbsp;</td><td class="tabev3">11</td><td class="tabev5" id="magtyp0" >ML</td><td class="tabev2">2.6</td><td id="reg0" class="tb_region" >&#160;NEAR THE COAST OF WESTERN TURKEY</td><td class="comment updatetimeno" id="upd0" style="text-align:right;">2016-10-15 07:28</td></tr>
-        flags=re.DOTALL
+        r';(?P<ura>(\d{2})):(?P<minuta>(\d{2})):(?P<sekunda>(\d{2}.\d))<.*?'
+        r'>(?P<sirina>(\d{2}.\d{2}))&.*?'
+        r'">(?P<SIR>([NS]))&.*?'
+        r'>(?P<dolzina>(\d{2}.\d{2}))&.*?'
+        r'">(?P<DOL>([WE]))&.*?'
+        r'">(?P<globina>(\d+))<.*?'
+        r'">(?P<magnituda>(\d.\d))</.*?'
+        r';(?P<regija>(.*?))</.*?'
+        #</td><td id="reg0" class="tb_region" >&#160;NEAR THE COAST OF WESTERN TURKEY</td><td class="comment updatetimeno" id="upd0" style="text-align:right;">2016-10-15 07:28</td></tr>
+        , flags=re.DOTALL
     )
 
     for html in orodja.datoteke('Potresi/'):
         for potres in re.finditer(regex_potresa, orodja.vsebina_datoteke(html)):
-            print(potres.group('sekunda'))
+            print(potres.group('id'))
 
-iskanje()
+def pocisti(potres):
+    podatki = potres.groupdict()
+    podatki['id'] = int(podatki['id'])
+    podatki['leto'] = int(podatki['leto'])
+    podatki['mesec'] = int(podatki['mesec'])
+    return podatki
+
+
+regex_potresa = re.compile(
+    r'<tr id="(?P<id>(\d{6}))".*?'
+    r'>(?P<leto>(\d{4}))-(?P<mesec>(\d{2}))-(?P<dan>(\d{2})).*?'
+    r';(?P<ura>(\d{2})):(?P<minuta>(\d{2})):(?P<sekunda>(\d{2}.\d))<.*?'
+    r'>(?P<sirina>(\d{2}.\d{2}))&.*?'
+    r'">(?P<SIR>([NS]))&.*?'
+    r'>(?P<dolzina>(\d{2}.\d{2}))&.*?'
+    r'">(?P<DOL>([WE]))&.*?'
+    r'">(?P<globina>(\d+))<.*?'
+    r'">(?P<magnituda>(\d.\d))</.*?'
+    r';(?P<regija>(.*?))</.*?'
+    # </td><td id="reg0" class="tb_region" >&#160;NEAR THE COAST OF WESTERN TURKEY</td><td class="comment updatetimeno" id="upd0" style="text-align:right;">2016-10-15 07:28</td></tr>
+    , flags=re.DOTALL
+)
+
+
+def izloci_podatke(imenik):
+    potresi = []
+    for html_datoteka in orodja.datoteke(imenik):
+        for potres in re.finditer(regex_potresa, orodja.vsebina_datoteke(html_datoteka)):
+            potresi.append(pocisti(potres))
+    return potresi
+
+#potresi = izloci_podatke('Potresi/')
+#orodja.zapisi_tabelo(potresi, ['id', 'leto', 'mesec', 'DOL', 'dolzina', 'regija', 'minuta', 'SIR', 'ura', 'sirina', 'dan', 'magnituda', 'globina', 'sekunda'], 'mogoce_potresi.csv')
